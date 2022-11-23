d2e0c7ed5dc623c7c5d793c2af75b8dfee125149
Marco <brandt.marco05@gmail.com>
2022-11-23 08:36:24 +0100

    sensor reading

---
+++ App/app/src/main/java/com/myproject/beschleunigungssensor/MainActivity.java
@@ -0,0 +1,59 @@
+package com.myproject.beschleunigungssensor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    TextView tv_x, tv_y, tv_z;
+
+    private SensorManager senSensorManager;
+    private Sensor senAccelerometer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv_x = findViewById(R.id.tv_x);
+        tv_y = findViewById(R.id.tv_y);
+        tv_z = findViewById(R.id.tv_z);
+
+
+        senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senSensorManager.registerListener(this, senAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Sensor mySensor = sensorEvent.sensor;
+
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = sensorEvent.values[0];
+            float y = sensorEvent.values[1];
+            float z = sensorEvent.values[2];
+
+            tv_x.setText("" + x);
+            tv_y.setText("" + y);
+            tv_z.setText("" + z);
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+
+
+
+}
